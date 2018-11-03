3ff5c2135a52ec525c467b8dbac51ef19e7d2193
Roei <roei86@gmail.com>
2018-11-03 16:40:19 -0400

    * Finiched EX4

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -73,7 +73,31 @@ J = 1 / m * sum(sum((-new_y .* log(h) - (1 - new_y) .* log(1 - h)))) + 1 / (2 * 
 %         Hint: We recommend implementing backpropagation using a for-loop
 %               over the training examples if you are implementing it for the 
 %               first time.
-%
+
+for t=1:m
+  % Step 1
+  a_1 = (A_1(t,:))'; % size(a_1) = 401x1
+  z_2 = Theta1 * a_1; % size(z_2) = 25x1
+  a_2 = [1 ; sigmoid(z_2)]; % size(A_2) = 26x1
+  z_3 = Theta2 * a_2; % size(z_2) = 10x1
+  a_3 = sigmoid(z_3); % size(a_3) = 10x1
+  
+  % Step 2
+  delta_3 = a_3 - new_y(t,:)'; % size(delta_3) = 10x1
+  
+  % Step 3
+  
+  delta_2 = Theta2' * delta_3 .* sigmoidGradient([1; z_2]); % size(delta_3) = 25x1
+  
+  % Step 4
+  delta_2 = delta_2(2:end);
+  Theta2_grad = Theta2_grad + delta_3 * a_2'; % Size(Theta2_grad) = 10x26
+	Theta1_grad = Theta1_grad + delta_2 * a_1'; % (25*1)*(1*401) = 25x401
+endfor
+ % Step 5
+ Theta1_grad /= m;
+ Theta2_grad /= m;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -81,8 +105,8 @@ J = 1 / m * sum(sum((-new_y .* log(h) - (1 - new_y) .* log(1 - h)))) + 1 / (2 * 
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + (lambda/m) * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + (lambda/m) * Theta2(:, 2:end);
 
 
 
